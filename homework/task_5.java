438d8ff9bba8d1e623c413823dc3eab4a3e1f93c
Богдан Заклунний <bohdanzaklunnyi@gmail.com>
2022-01-04 22:02:45 +0200

    changes

--- homework/task_5.java
+++ homework/task_5.java
@@ -1,9 +1,25 @@
 package homework;
+//import java.util.*;
 
 public class task_5 {
     public static void main(String[]args){
+        /**
+         * program generate random int in a specific range 20-70
+         */
         double a = Math.random() * 100;
-        System.out.println((int)a);
+        int random = (int)a;
+        if (random < 20){
+            random += 20;
+            System.out.println(random);
+        } else if(random > 70){
+            random -= 30;
+            System.out.println(random);
+        } else {
+            System.out.println(random);
+        }
+
+        //Date current_Date = new Date();
+        //System.out.println(current_Date);
 
         System.out.println("User: Zaklunnyi, added: 30.12.2021 11:49, resolved: 4.01.2022 21:17"); //info
     }
